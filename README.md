b7a8d0e908aa22a58ece64502602cd4214c1fe89
harnia-git <harnia67.23@gmail.com>
2023-08-17 14:38:51 +0900

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # gabor_test
+gabor_multi_img.py->"image{}.jpg".format(6+i)
+image6.jpg', 'image7.jpg', 'image8.jpg-> mergeimage.py -> merged_image3.jpg
+merged_image2.jpg', 'merged_image3.jpg', 'merged_image2.jpg-> mergeimage_col.py -> merged_imagecol.jpg
+merged_imagecol.png -> shift.py -> scaled_image.png
--- alpha.py
+++ alpha.py
@@ -16,20 +16,20 @@ image_with_alpha[:, :, 2] = image
 image_with_alpha[:, :, 3] = 128  # 透過度を設定（0は完全透明、255は完全不透明）
 
 # すべての画素の透過度を50%にする
-image_with_alpha[:, :, 3] = 128  # 透過度を設定（0は完全透明、255は完全不透明）
+#image_with_alpha[:, :, 3] = 128  # 透過度を設定（0は完全透明、255は完全不透明）
 
 # PNG画像として保存する
 output_path = 'merged_imagecol.png'
 cv2.imwrite(output_path, image_with_alpha)
 
 
-# 薄桃色のRGB値
-tint_color = (255, 218, 185)
+# # 薄桃色のRGB値
+# tint_color = (255, 218, 185)
 
-# 色の変換
-#colored_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)  # グレースケールからBGRへ変換
-colored_image = np.where(image_with_alpha < 200, image_with_alpha + np.array(tint_color, dtype=np.uint8), 255)  # しきい値以下の値に薄桃色を加算
+# # 色の変換
+# #colored_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)  # グレースケールからBGRへ変換
+# colored_image = np.where(image_with_alpha < 200, image_with_alpha + np.array(tint_color, dtype=np.uint8), 255)  # しきい値以下の値に薄桃色を加算
 
-# 修正された画像を保存
-output_path = 'colored_image.png'
-cv2.imwrite(output_path, colored_image)
+# # 修正された画像を保存
+# output_path = 'colored_image_alpha.png'
+# cv2.imwrite(output_path, colored_image)
